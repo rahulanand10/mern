0202c8e73d612dc80ea5dcd05776ade04fe3efb7
Rahul Anand <rahulanand0910@gmail.com>
2020-08-07 20:01:55 +0530

    Grunt Part-1

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports=function(grunt){
+
+    const sass = require('node-sass');
+
+    require('time-grunt')(grunt);
+
+    require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+        sass:{
+
+            options: {
+                implementation: sass,
+                sourceMap: true
+            },
+            dist:{
+                files:{
+                    'css/styles.css':'css/styles.scss'
+                }
+            }
+        },
+        watch:{
+            files:'css/*.scss',
+            tasks:['sass']
+        },
+        browserSync:{
+            dev:{
+                bsFiles:{
+                    scr:[
+                        'css.*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options:{
+                    watchTask:true,
+                    server:{
+                        baseDir:'./'
+                    }
+                }
+                
+            }
+        }
+
+    });
+
+    grunt.registerTask('css',['sass']);
+    grunt.registerTask('default',['browserSync','watch']);
+};
